18b1b777f9e81dd9fc86113b80095f14527d41c9
Benjamin Morel <benjamin.morel@gmail.com>
2015-07-04 23:48:18 +0200

    Initial commit of API documentation

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Brick\\Math\\BigDecimal"],["c","Brick\\Math\\BigInteger"],["c","Brick\\Math\\BigNumber"],["c","Brick\\Math\\BigRational"],["c","Brick\\Math\\Exception\\ArithmeticException"],["c","Brick\\Math\\Exception\\DivisionByZeroException"],["c","Brick\\Math\\Exception\\NumberFormatException"],["c","Brick\\Math\\Exception\\RoundingNecessaryException"],["c","Brick\\Math\\RoundingMode"]];
